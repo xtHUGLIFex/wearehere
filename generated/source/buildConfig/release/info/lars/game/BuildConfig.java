d97993ce435de53a5fbb60e4763401d00f9442ad
ErRommel <corsakov.alex@yandex.ru>
2017-02-27 18:31:53 +0300

    Add files via upload

---
+++ generated/source/buildConfig/release/info/lars/game/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package info.lars.game;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "info.lars.game";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
